b18ef356e7fc0830d3d79013b1b3865efa32aae4
Berkan Özgür <64234211+berkan-ozgur@users.noreply.github.com>
2023-02-14 17:07:32 +0300

    odev7 is committed.

---
+++ odev7/dvdrental7.sql
@@ -0,0 +1,20 @@
+--Merhabalar,
+
+---Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+--film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+SELECT rating FROM film
+GROUP BY rating;
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+SELECT replacement_cost, COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id;
+--city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+--Kolay Gelsin.
